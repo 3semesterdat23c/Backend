ca24ca99148675fe7e68ba779e9ec47a0374cf81
Aleksandergreg <alek3691@stud.kea.dk>
2024-11-20 09:32:28 +0100

    Initial commit

---
+++ src/test/java/org/example/backendclerkio/BackendClerkIoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendclerkio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClerkIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
