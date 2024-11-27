14e4f8c2b759b303abfadff16a71c7ce948f07a7
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-11-27 12:25:42 +0100

    Changed User.java to use first and last name instead of username

--- src/main/java/org/example/backendclerkio/entity/User.java
+++ src/main/java/org/example/backendclerkio/entity/User.java
@@ -19,8 +19,11 @@ public class User {
     @Column(name = "user_id")
     private int userId;
 
-    @Column(name = "username", nullable = false)
-    private String username;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
 
     @Column(name = "email", nullable = false)
     private String userEmail;
@@ -31,8 +34,9 @@ public class User {
     @Column(name = "passwordHash", nullable = false)
     private String passwordHash;
 
-    public User(String username, String userEmail, String passwordHash) {
-        this.username = username;
+    public User(String firstName, String lastName, String userEmail, String passwordHash) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.userEmail = userEmail;
         this.isAdmin = false;
         this.passwordHash = passwordHash;
