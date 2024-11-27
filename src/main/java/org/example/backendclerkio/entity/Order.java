c1009b5887809b65417ec44c4f02025377d06034
GustavSoederberg <144337665+GustavSoederberg@users.noreply.github.com>
2024-11-27 10:28:44 +0100

    Added relations in java, so that the MySQL databse is up to date with ER-Model

---
+++ src/main/java/org/example/backendclerkio/entity/Order.java
@@ -0,0 +1,32 @@
+package org.example.backendclerkio.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id", nullable = false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column (name = "order_date", nullable = false)
+    private long orderDate;
+
+    @Column (name = "is_paid", nullable = false)
+    private boolean isPaid;
+
+}
