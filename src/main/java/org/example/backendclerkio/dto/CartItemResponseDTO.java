e7f323961a7ffa29de38af5e7e5df4ef185ef19c
Aleksandergreg <alek3691@stud.kea.dk>
2024-12-06 08:44:30 +0100

    You can now see the discounted price in your cart.
    Still needs some UI-fix where you can see the original price as well

--- src/main/java/org/example/backendclerkio/dto/CartItemResponseDTO.java
+++ src/main/java/org/example/backendclerkio/dto/CartItemResponseDTO.java
@@ -1,3 +1,8 @@
 package org.example.backendclerkio.dto;
 
-public record CartItemResponseDTO(int productId, String productName, int quantity, double priceAtTimeOfOrder,double originalPrice, String productImageUrl) {}
+public record CartItemResponseDTO(int productId,
+                                  String productName,
+                                  int quantity,
+                                  double priceAtTimeOfOrder,
+                                  double originalPrice,
+                                  String productImageUrl) {}
